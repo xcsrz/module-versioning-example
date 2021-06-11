71da89247dc7992dff84919442e2677264b2b646
xcsrz <x@xcsrz.me>
2021-06-11 10:24:29 -0700

    creating v2 of [ Foo ] (step 2/2)

---
+++ components/Foo/v1/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css'
+
+const Foo = (props) => {
+    return (
+        <div className="foo">
+            <h2> Hello there {props.name}, I'm Foo!</h2>
+        </div>
+    );
+};
+
+export default Foo;
+
+
