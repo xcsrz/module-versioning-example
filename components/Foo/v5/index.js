314a2cf266b13e7ac93864bae8156a9df891fff8
xcsrz <x@xcsrz.me>
2021-06-11 10:25:17 -0700

    adding comment to track progress

--- components/Foo/v5/index.js
+++ components/Foo/v5/index.js
@@ -15,3 +15,4 @@ export default Foo;
 // migrated from 1 to 2 Fri Jun 11 10:24:29 PDT 2021
 // migrated from 2 to 3 Fri Jun 11 10:25:15 PDT 2021
 // migrated from 3 to 4 Fri Jun 11 10:25:16 PDT 2021
+// migrated from 4 to 5 Fri Jun 11 10:25:16 PDT 2021
