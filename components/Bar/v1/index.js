92dda643a11174ae7dab69e7825a6035982a7b65
xcsrz <x@xcsrz.me>
2021-06-11 10:25:07 -0700

    creating v2 of [ Bar ] (step 2/2)

---
+++ components/Bar/v1/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css'
+
+const Bar = (props) => {
+    return (
+        <div className="bar">
+            <h2> Hello there {props.name}, I'm Bar!</h2>
+        </div>
+    );
+};
+
+export default Bar;
+
+
