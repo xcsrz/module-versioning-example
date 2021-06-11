1a15ece0874007ae1ab11e48f648b9979808e124
xcsrz <x@xcsrz.me>
2021-06-11 10:25:16 -0700

    creating v4 of [ Foo ] (step 2/2)

---
+++ components/Foo/v3/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Person.css'
+
+const Foo = (props) => {
+    return (
+        <div className="foo">
+            <h2> Hello there {props.name}, I'm Foo!</h2>
+        </div>
+    );
+};
+
+export default Foo;
+
+
+// migrated from 1 to 2 Fri Jun 11 10:24:29 PDT 2021
+// migrated from 2 to 3 Fri Jun 11 10:25:15 PDT 2021
